ae092ac1044e62309dad80450af8ace0f1edabaf
chaericodes <133076413+chaericodes@users.noreply.github.com>
2023-12-30 20:10:48 +0800

    Creating a login card css and jsx file

---
+++ src/components/Login-card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+  return (
+    <>
+      <div className="login-card">
+        <div className="login-card-container">
+          <p>Hello</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
