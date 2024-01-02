a819bca79b7a0e99ab00d2a124c916ca5d7cd877
chaericodes <133076413+chaericodes@users.noreply.github.com>
2024-01-02 21:09:42 +0800

    Used React Routing for better navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { ReactDOM } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
+import SignUp from './components/SignUp'
 import './App.css'
 
 function App() {
-
-  return (
-    <div className="App">
-      <Login />
-    </div>
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<Login />} />
+      <Route path='SignUp' element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
