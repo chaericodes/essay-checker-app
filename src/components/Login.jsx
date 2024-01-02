2ceac8bda40c17c84d4f133143dd1a59d3cc2912
chaericodes <133076413+chaericodes@users.noreply.github.com>
2024-01-02 17:22:43 +0800

    Finalized Login Component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,37 +29,48 @@ function Login() {
               </div>
               <div className="row">
                 <div className="col login-card-element login-details">
-                  <label htmlFor="exampleFormControlInput1">Email</label>
-                  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder='' />
+                  <label htmlFor="exampleFormControlInput1" className="text-white">Email</label>
+                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder='' />
                 </div>
               </div>
               <div className="row">
                 <div className="col login-card-element login-details">
-                <label htmlFor="inputPassword">Password</label>
-                  <input type="password" class="form-control" id="inputPassword" />
+                <label htmlFor="inputPassword" className="text-white">Password</label>
+                  <input type="password" className="form-control" id="inputPassword" />
                 </div>
               </div>
               <div className="row">
                 <div className="col login-card-element">
-                  <a href="/">Forgot Password?</a>
+                  <a href="/" id="forgotPasswordLink">Forgot Password?</a>
                 </div>
               </div>
               <div className="row">
-                <div className="col login-card-element">
+                <div className="col login-card-element text-white">
                   - or -
                 </div>
               </div>
               <div className="row">
-                <div className="col login-card-element">
-                  Hello7
+                <div className="col login-card-element login-btns">
+                <button type="button" className="btn btn-light w-100">Continue with Google</button>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col login-card-element login-btns">
+                  <button type="button" className="btn btn-light w-100">Continue with Facebook</button>
                 </div>
               </div>
               <div className="row">
                 <div className="col login-card-element">
-                  Hello8
+                <button type="button" className="btn btn-light w-50 rounded" id="login-btn">Log In</button>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col login-card-element sign-up-question">
+                  <span className="question">Don't have any account?</span> <span><a href="#" id="sign-up-link">Sign up here.</a></span>
                 </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
