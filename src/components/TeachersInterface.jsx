d76a2d82a9b7bbff02d7c76c503bc81616d70f08
Eyiiiiii <133078366+Eyiiiiii@users.noreply.github.com>
2024-01-21 01:22:48 -0700

    added add-btn

--- src/components/TeachersInterface.jsx
+++ src/components/TeachersInterface.jsx
@@ -197,6 +197,26 @@ function TeachersInterface() {
                     </div>
                   </div>
                 </div>
+                <div className="col add-btn">
+                  {" "}
+                  <button type="button" id="add-btn">
+                    <link rel="stylesheet" href="" />{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6 plus"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
