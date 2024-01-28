ff9012bb506f2fa9558c88a402ccc38831fad825
angeeeeeeeelllaaa <122432567+angeeeeeeeelllaaa@users.noreply.github.com>
2024-01-28 20:57:05 +0800

    updating account info

--- src/components/AccountInfo.jsx
+++ src/components/AccountInfo.jsx
@@ -82,7 +82,7 @@ function AccountInfo() {
                 <div className="col">
                   {" "}
                   <button type="button" id="acc-btn">
-                    <Link to="" id="acc-lnk">
+                    <Link to="/accountInfo" id="acc-lnk">
                       Account Information
                     </Link>
                   </button>
@@ -146,65 +146,26 @@ function AccountInfo() {
               </div>
             </div>
           </div>
-
           <div
             className="col-9 main-content border border-success"
             style={{ height: "780px" }}
           >
-            <div className="row"></div>
-            <div className="container content">
-              <div className="row">
-                {" "}
-                <div className="col">
-                  <h3 id="head">Essay Checker</h3>
-                </div>
+          <div className="row"></div>
+          <div className="container account">
+            <div className="row">
+              <div className="col">
+              <img src="./src/assets/account.png" id="prof" />
+              <div className="col-4" id="info">
+                <p id="accountName">Angela Nicole Pinlac</p>
+                <p id="accountEmail">angelanicole.pinlac@tup.edu.ph</p> 
+                <hr />
+
               </div>
-            </div>
-            <div className="container questions">
-              <div className="row">
-                <div className="col-4 q-box">
-                  <div className="row">
-                    <div className="col label d-flex justify-content-end">
-                      QUESTION 01
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col view">
-                      <button
-                        type="button"
-                        id="view-btn"
-                        className="btn btn-light"
-                      >
-                        <Link to="/viewTeachersInterface" id="view-lnk">
-                          Should social media platforms be banned from
-                          collecting their users' data?
-                        </Link>
-                      </button>
-                    </div>
-                  </div>
-                  <div className="col line"></div>
-                  <div className="col bottom">
-                    <div className="row">
-                      <div className="col-2 status">Active:</div>
-                      <div className="col-5 section"> BSCS-NS-4A</div>
-                      <div className="col-5 copy-btn">
-                        <button
-                          type="button"
-                          id="copy-btn"
-                          className="btn btn-light"
-                        >
-                          <Link to="" id="copy-lnk">
-                            Copy Link
-                          </Link>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              </div>  
             </div>
           </div>
         </div>
+       </div>
       </div>
     </div>
   );
